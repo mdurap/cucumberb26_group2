110e93fbdb6d460c1d858afde7cce5df07f0cf76
mdurap <cndurap@gmail.com>
2022-07-01 21:19:58 -0600

    eating cucumber updated

--- src/test/java/com/cydeo/step_definitions/EatingCucumber.java
+++ src/test/java/com/cydeo/step_definitions/EatingCucumber.java
@@ -9,6 +9,14 @@ public class EatingCucumber {
     public void john_is_hungry() {
         System.out.println("It is from Given");
     }
+    @Given("{string} is hungry")
+    public void is_hungry(String name) {
+        System.out.println("It is from Given "+name+" is hungry");
+    }
+    @When("he eats {int} cucumbers")
+    public void he_eats_cucumbers(Integer quantity) {
+        System.out.println("he eats "+ quantity +" cucumbers");
+    }
     @When("he eats some cucumbers")
     public void he_eats_some_cucumbers() {
         System.out.println("It is from When");
--- src/test/resources/features/eating_cucumber.feature
+++ src/test/resources/features/eating_cucumber.feature
@@ -4,5 +4,10 @@ Feature: Eating too many cucumbers may not be good for you
   @eating
   Scenario: Eating a few is no problem
     Given John is hungry
-    When he eats some cucumbers
+    When he eats 3 cucumbers
+    Then he will be full
+
+  Scenario: Eating a few is no problem Another example
+    Given "Adam" is hungry
+    When he eats 6 cucumbers
     Then he will be full
