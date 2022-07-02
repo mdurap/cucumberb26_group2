906f35fc1a01e822619df267d4327912ca9b163d
mdurap <cndurap@gmail.com>
2022-07-01 21:36:00 -0600

    hook updated

--- src/test/java/com/cydeo/step_definitions/Hook.java
+++ src/test/java/com/cydeo/step_definitions/Hook.java
@@ -2,15 +2,20 @@ package com.cydeo.step_definitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 public class Hook {
     @Before
-    public void setup(){
+    public void setup(Scenario scenario){
         System.out.println("Running before the scenario");
+        System.out.println(scenario.getName());
+
     }
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario){
         System.out.println("Running after the scenario");
+        System.out.println(scenario.getName());
+        System.out.println(scenario.isFailed());
     }
 
 }
