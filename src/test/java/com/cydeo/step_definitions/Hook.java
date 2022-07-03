76a2fdf87b75aeb87ce8efc399358646688c746c
mdurap <cndurap@gmail.com>
2022-07-03 17:06:51 -0600

    Updated

--- src/test/java/com/cydeo/step_definitions/Hook.java
+++ src/test/java/com/cydeo/step_definitions/Hook.java
@@ -1,8 +1,11 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hook {
     @Before
@@ -16,6 +19,8 @@ public class Hook {
         System.out.println("---Running after the scenario---");
         System.out.println(scenario.getName());
         System.out.println(scenario.isFailed());
+        byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot, "image/png", scenario.getName());
     }
 
 }
--- src/test/resources/features/eating_cucumber.feature
+++ src/test/resources/features/eating_cucumber.feature
@@ -1,12 +1,13 @@
+@eating
 Feature: Eating too many cucumbers may not be good for you
   user story: Eating too much of anything may not be good for you
 
-  @eating
+
   Scenario: Eating a few is no problem
     Given John is hungry
     When he eats 3 cucumbers
     Then he will be full
-  @smoke
+
   Scenario: Eating a few is no problem Another example
     Given "Adam" is hungry
     When he eats 6 cucumbers
